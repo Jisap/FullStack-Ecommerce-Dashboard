8931103c6f3180a057786c05d0a89234d1bdb229
Jorge Matos <jisapjisap@gmail.com>
2023-09-08 18:03:40 +0200

    Navbar

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,6 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
  
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+export function cn(...inputs: ClassValue[]) { // La función cn() es un helper de Shadcn UI que se usa para combinar  
+  return twMerge(clsx(inputs))                // clases Tailwind CSS de forma condicional.
 }
