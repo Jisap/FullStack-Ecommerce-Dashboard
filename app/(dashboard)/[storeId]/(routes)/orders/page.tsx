618740b032dfad105a022fb5973a7f824e30ef7a
Jorge Matos <jisapjisap@gmail.com>
2023-09-25 18:41:37 +0200

    order

--- app/(dashboard)/[storeId]/(routes)/orders/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/page.tsx
@@ -1,25 +1,39 @@
 import prismadb from "@/lib/prismadb";
-import { BillboardClient } from "./components/client";
-import { BillboardColumn } from "./components/columns";
+import { OrderClient } from "./components/client";
+import { OrderColumn } from "./components/columns";
 import { format } from "date-fns";
+import { formatter } from "@/lib/utils";
 
 
 
 
-const BillboardsPage = async({ params }: { params: { storeId: string }}) => {
+const OrdersPage = async({ params }: { params: { storeId: string }}) => {
 
-  const billboards = await prismadb.billboard.findMany({
+  const orders = await prismadb.order.findMany({ // Cada order contendrá -> orderItems: orderItem[] -> product
     where: {
       storeId: params.storeId
+    },
+    include:{
+      orderItems:{
+        include: {
+          product: true
+        }
+      }
     },
     orderBy: {
       createdAt: 'desc'
     }
   });
 
-  const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
+  const formattedOrders: OrderColumn[] = orders.map((item) => ({
     id: item.id,
-    label: item.label,
+    phone: item.phone,
+    address: item.address,
+    products: item.orderItems.map((orderItem) => orderItem.product.name).join(', '),
+    totalPrice: formatter.format(item.orderItems.reduce((total, item) => {
+      return total + Number(item.product.price)
+    }, 0)),
+    isPaid: item.isPaid,
     createdAt: format(item.createdAt, "MMMM do, yyyy")
   }));
 
@@ -27,10 +41,10 @@ const BillboardsPage = async({ params }: { params: { storeId: string }}) => {
     <div className="flex-col">
         <div className="flex-1 space-y-4 p-8 pt-6">
             {/* billboards -> client -> dataTable -> se basa en column y cell-action */}
-            <BillboardClient data={formattedBillboards}/>
+            <OrderClient data={formattedOrders}/>
         </div>
     </div>
   )
 }
 
-export default BillboardsPage;
+export default OrdersPage;
